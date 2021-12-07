c9a4bf1eff2f422c058ab17cea3274b2b64e79f6
AdityaGokhale23 <aditya.gokhale06@gmail.com>
2021-12-07 16:48:43 +0530

    upload

---
+++ asp.net_crud-master/SQLQuery3.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO [data].
+[catMaster] ([CatId], [CategoryName],) VALUES (1, N'Computer')  
+INSERT INTO [data].
+[catMaster] ([CatId], [CategoryName],) VALUES (2, N'Storage')  
+INSERT INTO [data].
+[catMaster] ([CatId], [CategoryName],) VALUES (3, N'Acssories')  
+INSERT INTO [data].
+[catMaster] ([CatId], [CategoryName],) VALUES (4, N'Digital Camera')  
+INSERT INTO [data].
+[catMaster] ([CatId], [CategoryName],) VALUES (5, N'Camera Aceesories')  
